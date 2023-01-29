0c0dca36e5491acb27ef6a9c5e4f8f18dd021bd3
Sergey <blackwhiterainbow@yandex.ru>
2023-01-29 19:20:18 +0300

    add Возвеение в степень

---
+++ Work25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+
+int result(int A, int B)
+{
+    int num = A;
+    for(int i = 1; i < B; i++)
+    {
+        num = num*A;
+    }
+       
+    return num;
+    
+}
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(result(A, B));
