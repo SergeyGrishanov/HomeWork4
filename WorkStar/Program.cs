644c14981a5c9c600b94c829303cefd7a4d1fb1e
Sergey <blackwhiterainbow@yandex.ru>
2023-01-29 19:22:36 +0300

    add Нарисовать елочку

---
+++ WorkStar/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите функцию, которая принимает одно число - высоту елочки и рисует ее в консоли звездочками.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int heigth = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < heigth; i = i + 2)
+{
+    Console.SetCursorPosition(heigth - i, i + 1);
+   
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write("*");
+        Console.Write(" ");
+    }
+}
